b2a97042436385fdcdf7affff1348c2d4d90a083
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-13 10:11:49 +0100

    Step 2 completed.
    Phone class created with properties

---
+++ JasonJordan_S00188447/JasonJordan_S00188447/Phone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JasonJordan_S00188447
+{
+
+   
+
+
+    class Phone
+    {
+        public int Price { get; set; }
+
+        public string Name { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        public string OS_Image { get; set; }
+
+        public string Phone_Image { get; set; }
+
+
+
+    }
+}
